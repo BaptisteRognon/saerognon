8d01db4b9aacc559097b6e52048a7bfb10162907
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-05-30 13:13:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    
+    <nav>
+      <RouterLink to="/" class="text-blue-600 underline">Accueil</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+
+export default {
+  components: {  },
+};
+</script>
