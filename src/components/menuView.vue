471c34ec179f6b603e2e0938256dd9f46bd809d1
Baptiste0715 <baptiste.rognon@edu.univ-fcomte.fr>
2022-06-01 17:49:05 +0200

    changement footer et composant

--- src/components/menuView.vue
+++ src/components/menuView.vue
@@ -3,26 +3,27 @@
       <div class="bg-transparent">
         <div class="max-w-7xl mx-auto">
             <div class="flex justify-between">
-                <!-- Menu -->
+                
                 <div class="flex space-x-4 justify-between">
-                    <!-- Logo -->
+                    
                     <div class="flex items-center justify-between py-5 px-2">
                         <RouterLink to="/"><img src="../assets/lys.svg"></RouterLink>
                     </div>
-                    <!-- Menu -->
+                    
                     <div class="hidden text-white ipad_pro:flex items-center space-x-1">
-            <RouterLink to="/" class="py-5 px-2 hover:font-bold">Accueil</RouterLink>
+            
             <RouterLink to="/artistes" class="py-5 px-2 hover:font-bold">Artistes</RouterLink>
             <RouterLink to="/contact" class="py-5 px-2 hover:font-bold">Contact</RouterLink>
             <RouterLink to="/festival" class="py-5 px-2 hover:font-bold">Festival</RouterLink>
             <RouterLink to="/mentionlegale" class="py-5 px-2 hover:font-bold">Mention Légale</RouterLink>
             <RouterLink to="/programme" class="py-5 px-2 hover:font-bold ">Programme</RouterLink>
+            <router-link to="/guidedestyle" class="py-5 px-2 hover:font-bold ">Guide de style</router-link>
                     
                     </div>
                 </div>
                
 
-                <!-- Bouton menu -->
+                
                 <div class="ipad_pro:hidden flex items-center">
                     <button @click="menuVisible = !menuVisible" class=" py-5 px-2 "> 
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" viewBox="0 0 20 20" fill="currentColor">
@@ -34,9 +35,9 @@
             </div>
         </div>
       </div>
-      <!-- Menu mobile -->
+      
       <div class="text-white bg-transparent ipad_pro:hidden" :class="{hidden: menuVisible}">
-      <RouterLink to="/" class="py-5 px-2 hover:font-bold">Accueil</RouterLink>
+      
             <RouterLink to="/artistes" class="block py-2 px-4  bg-transparent">Artistes</RouterLink>
             <RouterLink to="/contact" class="block py-2 px-4  bg-transparent">Contact</RouterLink>
             <RouterLink to="/festival" class="block py-2 px-4  bg-transparent">Festival</RouterLink>
---
+++ src/views/GuideView.vue
@@ -0,0 +1,22 @@
+<template>
+<header-view></header-view>
+    <div name="guide de style" class="bg-blanc m-5 p-3 rounded">
+        <p class="font-raleway text-dnonf">GUIDE DE STYLE</p>
+    </div>
+
+    <div name="guide de style" class="bg-blanc m-5 p-3 rounded">
+        <p class="font-raleway text-dnonf">POLICES</p>
+    </div>
+
+    <div name="guide de style" class="bg-blanc m-5 p-3 rounded">
+        <p class="font-raleway text-dnonf">COULEURS</p>
+    </div>
+
+        <div class="bg-blanc border-blanc mx-5 my-5">
+            <figure class=""></figure>
+        </div>
+
+    <div name="guide de style" class="bg-blanc m-5 p-3 rounded">
+        <p class="font-raleway text-dnonf">COMPOSANTS</p>
+    </div>
+</template>
